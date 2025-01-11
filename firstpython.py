b477367cc283a1bcfd03335efa30e240fadfddba
Moeez Ul Hassan <moeezjutt11@gmail.com>
2025-01-11 18:43:37 +0500

    Added small code

--- firstpython.py
+++ firstpython.py
@@ -8,3 +8,7 @@ name = input("Enter your name: ")
 for i in range(len(name)):
 	name[i] = input(f"Enter character for position {i}: ")
  
+#create a loop for rollnumbers
+rollno = input("Enter your rollno.: ")
+for i in range(len(rollno)):
+	rollno[i] = input(f"Enter character for position {i}: ")
